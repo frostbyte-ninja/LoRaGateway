848dcaa9cf5fea7297bb0d9199285a4f723d4cf3
frostbyte-ninja <117345621+frostbyte-ninja@users.noreply.github.com>
2025-08-12 12:34:10 +0200

    feat: add more logging on wifi reconnect

--- lib/mqtt/src/MqttClient.cpp
+++ lib/mqtt/src/MqttClient.cpp
@@ -63,6 +63,7 @@ MqttClient::wifiReconnect() const
     return;
   }
 
+  Serial.println(F("WiFi disconnected, reconnecting..."));
   WiFi.disconnect();
   WiFi.begin(m_ssid, m_wifiPassword);
   while (WiFi.status() != WL_CONNECTED) { // NOLINT (*-static-accessed-through-instance)
